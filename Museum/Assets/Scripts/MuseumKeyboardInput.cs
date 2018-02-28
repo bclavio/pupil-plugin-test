a0cec5702940ebd9153fd2234d9bec81e62d8e59
BICLAVIO\BiancaClavio <biclavio@gmail.com>
2018-02-28 11:18:26 +0100

    Adding the Museum scene

---
+++ Museum/Assets/Scripts/MuseumKeyboardInput.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MuseumKeyboardInput : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SetMuseumToCamera();
+    }
+
+    private void SetMuseumToCamera()
+    {
+        transform.rotation = new Quaternion(0, Camera.main.transform.rotation.y, 0, Camera.main.transform.rotation.w);
+        transform.position = Camera.main.transform.position;
+        transform.Translate(new Vector3(0, 0, 5));
+    }
+}
